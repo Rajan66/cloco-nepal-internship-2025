ee29768cfe5d637a8f3e24d76d94a37efe53a3ab
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-17 14:36:36 +0545

    feat: add templates, views and urls

--- 2025-02-17/myapp/main/views.py
+++ 2025-02-17/myapp/main/views.py
@@ -1,6 +1,29 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello world, this is my django app")
+    # orders by recent first and only shows the first 5 items
+    latest_question_list = Question.objects.order_by("-published_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "main/index.html", context)
+
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "main/details.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "main/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    response = "You're voting on question:"
+    return HttpResponse(f"{response} {question_id}")
