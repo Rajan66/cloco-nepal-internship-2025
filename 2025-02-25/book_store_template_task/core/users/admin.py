5e36e5066b56511340d9efe89e7940f7d2994a1c
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-26 16:35:05 +0545

    feat: add user model

--- 2025-02-25/book_store_template_task/core/users/admin.py
+++ 2025-02-25/book_store_template_task/core/users/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
-from .models import CustomUser as User
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm, AuthorCreationForm, AuthorChangeForm
+from .models import Author, CustomUser
 
-# Register your models here.
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password", "phone")}),
+        ("Permissions", {"fields": ("is_staff",
+         "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+         ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    add_form = AuthorCreationForm
+    form = AuthorChangeForm
+    model = Author
+    list_display = ("user", "birth_date")
+    list_filter = ("user",)
+    ordering = ("user",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Author, AuthorAdmin)
--- 2025-02-25/book_store_template_task/core/users/models.py
+++ 2025-02-25/book_store_template_task/core/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _  # marks strings for translation
 from django.utils import timezone
@@ -8,11 +8,6 @@ from core.base.models import BaseModel
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin, BaseModel):
-    class Meta:
-        # set table's name
-        verbose_name = "user"
-        verbose_name_plural = "users"
-
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(_("first name"), blank=True)
     last_name = models.CharField(_("first name"), blank=True)
@@ -20,11 +15,35 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, BaseModel):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+    created_at = None
 
     USERNAME_FIELD = "email"  # defines the unique identifer for the User model
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
+    class Meta:
+        # set table's name
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
     def __str__(self):
-        return self.email
+        name = self.first_name + " " + self.last_name
+        if (name == " "):
+            return self.email
+        return name
+
+
+class Author(BaseModel):
+    # default: user.author
+    # with related_name: user.author_profile
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        name = self.user.first_name + " " + self.user.last_name
+        if (name == " "):
+            return self.user.email
+        return name
