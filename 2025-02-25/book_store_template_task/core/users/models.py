f812da7596f5a1a3d2feab419c261addd506fa48
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-26 10:09:29 +0545

    feat: add user app and tests

--- 2025-02-25/book_store_template_task/core/users/models.py
+++ 2025-02-25/book_store_template_task/core/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+from .managers import BaseUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"),unique=True) 
+    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
