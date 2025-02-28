3d94cf83911c90a2d1d7a98d2a25a96812f67bda
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-28 11:27:56 +0545

    fix: update base models field

--- 2025-02-27/bookstore_drf/core/core/base/models.py
+++ 2025-02-27/bookstore_drf/core/core/base/models.py
@@ -6,7 +6,7 @@ class AbstractModel(models.Model):
     id = models.CharField(default=uuid, primary_key=True,
                           max_length=22, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         abstract = True
