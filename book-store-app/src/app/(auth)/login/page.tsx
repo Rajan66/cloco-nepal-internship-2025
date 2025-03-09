388af3b8da5c202d377ae6ea086a61143f89bbc9
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-09 22:07:36 +0545

    login form

---
+++ book-store-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/features/auth/components/LoginForm";
+import React from "react";
+
+// this is '/' route, change the route after adding homepage
+const page = () => {
+  return (
+    <div className="">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ book-store-app/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+
+const LoginForm = () => {
+  const form = useForm();
+
+  const onSubmit = () => {
+    console.log("hello worldo");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-3xl mx-auto py-10"
+      >
+        <FormField
+          control={form.control}
+          name="name_3661181449"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type="" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_0801452216"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="Placeholder" {...field} />
+              </FormControl>
+              <FormDescription>Enter your password.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+export default LoginForm;
